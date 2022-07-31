a517ba0d9c478bd7bd72c36cd1ef6f7a50a343e7
Carlos Zabala <czabala847@gmail.com>
2022-07-31 10:48:05 -0500

    Añadiendo navegacion entre paginas con useNavigate

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  return <h1>Login</h1>;
+  const navigate: NavigateFunction = useNavigate();
+
+  const onLogin = () => {
+    navigate("/", {
+      replace: true,
+    });
+  };
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
