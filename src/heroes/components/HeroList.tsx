d0c55697f9b3a30773a0480819f71768525afbc9
Carlos Zabala <czabala847@gmail.com>
2022-07-31 14:38:17 -0500

    Creando Card para mostrar los heroes

--- src/heroes/components/HeroList.tsx
+++ src/heroes/components/HeroList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { HeroCard } from "./HeroCard";
+
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 import { Hero, Publisher } from "../interfaces/Hero.interface";
 
@@ -11,10 +13,10 @@ export const HeroList: React.FC<Prop> = ({ publisher }) => {
   const heroes: Hero[] = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row row-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard key={hero.id} hero={hero} />
       ))}
-    </ul>
+    </div>
   );
 };
