20230ff42ca48671ceff502bc0496c65f3b7b071
Carlos Zabala <czabala847@gmail.com>
2022-07-31 15:06:12 -0500

    Estilos en la vista del detalle de un heroe

--- src/heroes/pages/HeroPage.tsx
+++ src/heroes/pages/HeroPage.tsx
@@ -1,20 +1,60 @@
 import React from "react";
-import { Navigate, useParams } from "react-router-dom";
+import {
+  Navigate,
+  NavigateFunction,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import { getHeroById } from "../helpers/getHeroById";
 import { Hero } from "../interfaces/Hero.interface";
 
 export const HeroPage: React.FC = () => {
+  const navigate: NavigateFunction = useNavigate();
   const { id } = useParams();
+  const hero: Hero | undefined = getHeroById(id!);
 
-  let hero: Hero | undefined = getHeroById(id!);
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
     return <Navigate to="/" />;
   }
 
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/${hero.id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group list-group-item">
+            <b>Alter ego:</b>
+            {hero.alter_ego}
+          </li>
+          <li className="list-group list-group-item">
+            <b>Publisher:</b>
+            {hero.publisher}
+          </li>
+          <li className="list-group list-group-item">
+            <b>First Appearance:</b>
+            {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Back
+        </button>
+      </div>
+    </div>
   );
 };
