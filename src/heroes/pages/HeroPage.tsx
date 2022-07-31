5e7993c320cd659dd5fd7e2f6480fd3ae93addae
Carlos Zabala <czabala847@gmail.com>
2022-07-31 14:57:08 -0500

    Obteniendo el id del heroe de la url con el hook useParams

--- src/heroes/pages/HeroPage.tsx
+++ src/heroes/pages/HeroPage.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
+import { Hero } from "../interfaces/Hero.interface";
 
-export const HeroPage = () => {
-  return <div>HeroPage</div>;
+export const HeroPage: React.FC = () => {
+  const { id } = useParams();
+
+  let hero: Hero | undefined = getHeroById(id!);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
+  );
 };
--- src/heroes/routes/HeroRoutes.tsx
+++ src/heroes/routes/HeroRoutes.tsx
@@ -13,7 +13,7 @@ export const HeroRoutes: React.FC = () => {
           <Route path="marvel" element={<MarvelPage />} />
           <Route path="dc" element={<DcPage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="hero" element={<HeroPage />} />
+          <Route path="hero/:id" element={<HeroPage />} />
 
           <Route path="/" element={<Navigate to="marvel" />} />
         </Routes>
